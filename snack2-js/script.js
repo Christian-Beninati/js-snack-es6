5675180fc98c2ac12ccfc6260141fa095b7ccba9
Christian-Beninati <christianbeninati.info@gmail.com>
2023-06-13 15:49:40 +0100

    SNACK 2

--- snack2-js/script.js
+++ snack2-js/script.js
@@ -1,1 +1,69 @@
-console.log('JS OK SNACK 2')
+console.log('JS SNACK 2')
+
+// Creo una Array di oggetti
+const students = [
+{
+    id: 213,
+    name: 'Marco della Rovere',
+    grades: 78,
+},
+{
+    id: 110,
+    name: 'Paola Cortellessa',
+    grades: 96,
+},
+{
+    id: 250,
+    name: 'Andrea Mantegna',
+    grades: 48,
+ },
+{
+    id: 145,
+    name: 'Gaia Borromini',
+    grades: 74,
+ },
+{
+    id: 196,
+    name: 'Luigi Grimaldello',
+    grades: 68,
+ },
+ {
+    id: 102,
+    name: 'Piero della Francesca',
+    grades: 50,
+ },
+ {
+    id: 120,
+    name: 'Francesca da Polenta',
+    grades: 84,
+},
+];
+
+// Creo una nuova array Filtrando gli studenti con un totale di voti superiore a 70
+
+// const studentsAbove70 = students.filter((student) => {
+//     return student.grades > 70;
+//   });
+  const studentsAbove70 = students.filter((student) => student.grades > 70);
+console.log('Studenti con un totale di voti superiore a 70:');
+console.log(studentsAbove70);
+
+
+// Creo una nuova array Filtrando gli studenti con un totale di voti superiore a 70 e id superiore a 120
+
+// const studentsAbove70IdAbove120 = students.filter((student) => {
+//     return student.grades > 70 && student.id > 120;
+//   });
+const studentsAbove70IdAbove120 = students.filter((student) => student.grades > 70 && student.id > 120);
+console.log('Studenti con un totale di voti superiore a 70 e id superiore a 120:');
+console.log(studentsAbove70IdAbove120);
+
+
+// Creo una nuova array contenente i nomi degli studenti in maiuscolo
+
+// const uppercaseNames = students.map((student) => {
+//     return student.name.toUpperCase();
+//   });
+const uppercaseNames = students.map((student) => student.name.toUpperCase());
+console.log('Nomi degli studenti in maiuscolo:');
+console.log(uppercaseNames);
--- steps.md
+++ steps.md
@@ -16,3 +16,35 @@ Generiamo e stampiamo in console la lista per i segnaposto.
 - Aggiungo alla nuova lista
 - Itero attraverso l'array con il metodo foEach e stampo in console
 - FINE
+
+<br>
+<br>
+
+# SNACK 2
+
+IAbbiamo un elenco degli studenti di una facoltà, identificati da id, Nome e somma totale dei loro voti di esame...
+Per preparare l'aula di un nuovo corso, dobbiamo svolgere una serie di operazioni
+
+1. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
+2. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
+3. Dobbiamo stampare le targhe col nome degli studenti: creare una lista contenente il loro nome tutto in maiuscolo ES (Marco della Rovere => MARCO DELLA ROVERE);
+   Questo è l'elenco degli studenti:
+
+Id Name Grades <br>
+213 Marco della Rovere 78 <br>
+110 Paola Cortellessa 96 <br>
+250 Andrea Mantegna 48 <br>
+145 Gaia Borromini 74 <br>
+196 Luigi Grimaldello 68 <br>
+102 Piero della Francesca 50 <br>
+120 Francesca da Polenta 84 <br>
+
+<br>
+<br>
+
+- INIZIO
+- Creo un Array di oggetti
+- Filtro gli studenti con un totale di voti superiore a 70 utilizzando il metodo filter. (Creo una nuova Array) e stampo in console
+- Filtro gli studenti con un totale di voti superiore a 70 e un ID superiore a 120 utilizzando il metodo filter. (Creo una nuova Array) e stampo in console
+- Creo una nuova array utilizzando il metodo map per trasformare i nomi degli studenti in maiuscolo e stampo in console
+- FINE
